cbe6e48766bde167e4ddb4fcbec7453b57579aa0
sampath-amatam <sampath.amatam@gmail.com>
2020-05-24 23:45:43 -0400

    springboot and REST with Eclipse

---
+++ RestWithSpringBoot Eclipse/src/main/java/us/com/sampath/GreetingsController.java
@@ -0,0 +1,20 @@
+package us.com.sampath;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+	
+	private static final String template = "Hello, %s";
+	private final AtomicLong counter =  new AtomicLong();
+	
+	@RequestMapping("/greetings")
+	public Greetings greetings(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Greetings(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
---
+++ RestWithSpringBoot Eclipse/src/main/java/us/com/sampath/startup.java
@@ -0,0 +1,16 @@
+package us.com.sampath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(startup.class, args);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
